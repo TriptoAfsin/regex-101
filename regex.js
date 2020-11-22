4080fff18220b3ad5c65f4bf75cf71c089d53cea
TriptoAfsin <afsintripto@gmail.com>
2020-11-22 20:15:39 +0600

    added grouoing

--- regex.js
+++ regex.js
@@ -6,3 +6,13 @@ const searchQuery = /tripto/gm; //this is the search query, here we search for "
 const isPresent = searchQuery.test(myStr); //here we check , if present then true, else false
 
 console.log(isPresent);
+
+
+// grouping 
+
+const myName = /My name is ([a-z]+)/gm;
+
+const getMyName = myName.exec(myStr);
+
+console.log(getMyName); //will return an object
+console.log(getMyName[1]); // will return the name
